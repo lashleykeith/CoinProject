4f449eca7bafd369311185ac154c7eaebedbba65
Keith Lashley <lashleykeith@gmail.com>
2022-11-10 18:26:08 +0900

    Add files via upload

---
+++ CoinProjectGitHub/flone-react-ecommerce-template-2022-02-21-23-53-49-utc/flone/src/components/header/Logo.js
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Logo = ({ imageUrl, logoClass }) => {
+  return (
+    <div className={`${logoClass ? logoClass : ""}`}>
+      <Link to={process.env.PUBLIC_URL + "/"}>
+      <h3>Fine Bullions</h3>
+      </Link>
+    </div>
+  );
+};
+
+Logo.propTypes = {
+  imageUrl: PropTypes.string,
+  logoClass: PropTypes.string
+};
+
+export default Logo;
