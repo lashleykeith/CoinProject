341782d442d7621edf7725f60004a6bffdeceb52
Keith Lashley <lashleykeith@gmail.com>
2022-11-10 18:24:59 +0900

    Add files via upload

---
+++ CoinProjectGitHub/flone-react-ecommerce-template-2022-02-21-23-53-49-utc/flone/src/components/footer/FooterCopyright.js
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const FooterCopyright = ({ footerLogo, spaceBottomClass, colorClass }) => {
+  return (
+    <div
+      className={`copyright ${spaceBottomClass ? spaceBottomClass : ""} ${colorClass ? colorClass : ""
+        }`}
+    >
+      <div className="footer-logo">
+        <Link to={process.env.PUBLIC_URL + "/"}>
+          <h3>Fine Bullions</h3>
+        </Link>
+      </div>
+      <p>
+        &copy; {new Date().getFullYear()}{" "}
+        <a
+          href="https://hasthemes.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Fine Bullions LLC
+        </a>
+        .<br /> All Rights Reserved
+      </p>
+    </div>
+  );
+};
+
+FooterCopyright.propTypes = {
+  footerLogo: PropTypes.string,
+  spaceBottomClass: PropTypes.string,
+  colorClass: PropTypes.string
+};
+
+export default FooterCopyright;
